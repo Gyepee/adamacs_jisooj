eff7f2734417bd50397d1b8cdb277b39820c7673
Tobias Rose <tobias.rose@ukbonn.de>
2023-03-29 13:30:41 +0000

    added headfixed / free to Equipment schema

--- adamacs/schemas/equipment.py
+++ adamacs/schemas/equipment.py
@@ -19,7 +19,35 @@ class Equipment(dj.Manual):
     scanner_description=''         : varchar(255)
     """
 
+@schema
+class Restraint(dj.Manual):
+    definition = """
+    restraint                        : varchar(32)
+    ---
+    restraint_description=''         : varchar(255)
+    """
+
+@schema
+class Device(dj.Manual):
+    definition = """
+    camera                        : varchar(32)
+    ---
+    camera_description=''         : varchar(255)
+    """
 
 # CB DEV NOTE: I suggest adding Device here as a foreign key for DLC camera and then
 #              changing the reference in adamacs.pipeline. Currently, dlc relies on
 #              the above 'scanner' table for camera information, see model_videos.csv
+
+
+
+restraint_data = [
+ {'restraint': 'headfixed',
+  'restraint_description': 'headfixed recording',
+ },
+ {'restraint': 'unrestrained',
+  'restraint_description': 'freely moving recording',
+ }
+]
+
+Restraint.insert(restraint_data, skip_duplicates=True)
